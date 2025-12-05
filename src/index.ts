05faa5711c72d3bcb5c55bbc037e323b599f8437
The-FerryMan1 <mercedroxy@gmail.com>
2025-12-05 13:38:16 +0800

    env updated 2

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,7 @@ const app = new Elysia({ prefix: "/api" })
   .use(productService)
   .use(lineItems)
   .use(invoices)
-  .listen(3000);
-
+  .listen(Bun.env.PORT! || 3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
